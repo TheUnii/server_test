334c30c2be12e07701db299327957d000e0e4d44
TheUni <ivanpetera@mail.ru>
2026-02-12 13:58:32 +0300

    pract

---
+++ pract10/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 4000
+
+app.set("view engine", "ejs");
+
+
+app.get("/", (req, res) => {
+    res.render('form');
+})
+
+app.use(express.urlencoded())
+
+app.post("/letter", (req, res) => {
+    const formData = req.body
+    res.render("letter", formData)
+})
+
+
+app.listen(port, () => {
+  console.log(`Сервер запущен`)
+})
