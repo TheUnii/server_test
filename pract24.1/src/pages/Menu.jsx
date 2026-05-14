d2d75cf305134ef257ea186acbf0f22420492bde
TheUni <ivanpetera@mail.ru>
2026-05-14 16:17:20 +0300

    pract24.1

---
+++ pract24.1/src/pages/Menu.jsx
@@ -0,0 +1,12 @@
+export default function Menu() {
+    return (
+        <div>
+            <h2>Наше меню</h2>
+            <ul>
+                <li>Маргарита — 500 руб.</li>
+                <li>Пепперони — 600 руб.</li>
+                <li>Четыре сыра — 700 руб.</li>
+            </ul>
+        </div>
+    );
+}
